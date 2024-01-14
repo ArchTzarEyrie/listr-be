66ef620bd661cc1c28248f8f56903aa39cb0c39b
Zack <zack@Zacks-MacBook-Pro.local>
2024-01-14 17:55:51 -0500

    request to db works

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,13 +17,31 @@ router.options('/test', function(req, res, next) {
   res.send(200);
 })
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'myc00lP4s5word',
+  database: 'listr'
+})
+
 router.get('/test', function(req, res, next) {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
-    "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS, DELETE"
+    res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+      "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS, DELETE"
+    });
+    connection.connect()
+
+    connection.query('SELECT * from listrTestTable', (err, rows, fields) => {
+        if (err) throw err;
+
+        console.log('The response is: ', rows[0].data);
+
+        res.send(rows[0].data);
+    })
+
+    connection.end()
   });
-  res.send("Hanky Spanky");
-});
+
 
 module.exports = router;
