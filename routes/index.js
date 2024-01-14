666b7bfa5a165f8d962547f0e891e7ee15e8fa93
Zack <zack@Zacks-MacBook-Pro.local>
2024-01-14 16:58:50 -0500

    add headers for sample option and get for test request

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.options('/test', function(req, res, next) {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS, DELETE"
+  });
+  res.send(200);
+})
+
+router.get('/test', function(req, res, next) {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS, DELETE"
+  });
+  res.send("Hanky Spanky");
+});
+
 module.exports = router;
