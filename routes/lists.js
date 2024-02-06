55ba8329cb08bb1696b0694f7ff8f845ee387375
Zack <zack@Zacks-MacBook-Pro.local>
2024-02-06 14:45:16 -0500

    add proper list endpoint

---
+++ routes/lists.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var sqlConnector = require('../database/mysqlConnector');
+
+router.post('/create', async function(req, res) {
+  const connection = await sqlConnector.getConnection();
+
+  connection.connect();
+
+  try {
+    const sql = `INSERT INTO lists (owner, name) values ('${req.body.owner}, ${req.body.listName}')`;
+    await connection.query(sql);
+    res.sendStatus(200);
+    connection.end();
+  } catch (err) {
+    console.log(err);
+    connection.end();
+  }
+});
+
+module.exports = router;
