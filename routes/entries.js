78e3115be99ee632e83edb7375c3a1be6ea6bf2b
Zack <zack@Zacks-MacBook-Pro.local>
2024-03-08 18:40:01 -0500

    add endpoint to fetch entries

--- routes/entries.js
+++ routes/entries.js
@@ -8,14 +8,28 @@ router.post('/create', async function(req, res) {
   connection.connect();
 
   try {
-    const sql = `INSERT INTO entries (content, creator, listId, link) values ('${req.body.content}, ${req.body.creator}, ${req.body.listId}, ${req.body.link}')`;
-    await connection.query(sql);
-    res.sendStatus(200);
-    connection.end();
+    const sql = `INSERT INTO entries (content, creator, listId, link) values ('${req.body.content}', ${req.body.creator}, ${req.body.listId}, '${req.body.link}')`;
+    const [result, fields] = await connection.query(sql);
+    res.send({insertId: result.insertId});
   } catch (err) {
     console.log(err);
-    connection.end();
   }
+  connection.end();
+});
+
+router.get('/:listId', async function(req, res) {
+  const connection = await sqlConnector.getConnection();
+
+  connection.connect();
+
+  try {
+    const sql = `SELECT * FROM entries WHERE listId = ${req.params.listId}`;
+    const [rows, fields] = await connection.query(sql);
+    res.send(rows);
+  } catch (err) {
+    console.log(err);
+  }
+  connection.end();
 });
 
 module.exports = router;
